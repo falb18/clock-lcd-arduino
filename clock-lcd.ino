95349a0fb831a9419dc518afe76a24a1e6290d04
falb18 <arturo.lopezbo@gmail.com>
2023-04-04 18:50:00 -0600

    Add "day of week" parameter to display it on the LCD

--- clock-lcd.ino
+++ clock-lcd.ino
@@ -21,14 +21,23 @@
 /* Application macros: */
 
 /* The total number of editable paramaters in date and time strings */
-#define NUM_DATE_TIME_PARAMS 5
+#define NUM_DATE_TIME_PARAMS 6
 
 #define DATE_IDX 0
 #define MONTH_IDX 1
 #define YEAR_IDX 2
-#define HOUR_IDX 3
-#define MIN_IDX 4
-#define SEC_IDX 5
+#define WEEK_IDX 3
+#define HOUR_IDX 4
+#define MIN_IDX 5
+#define SEC_IDX 6
+
+#define MONDAY_IDX 0
+#define TUESDAY_IDX 1
+#define WENSDAY_IDX 2
+#define THURSDAY_IDX 3
+#define FRIDAY_IDX 4
+#define SATURDAY_IDX 5
+#define SUNDAY_IDX 6
 
 /* Each flag determines if it needs to be updated on the RTC module or not */
 #define UPDATE_DATE 0x01
@@ -66,6 +75,16 @@ bool h12_flag = HR_12_FRMT;
 bool pm_flag = PM_FRMT;
 bool century = false;
 
+char *str_days_week[] = {
+    "MON",
+    "TUE",
+    "WEN",
+    "THU",
+    "FRI",
+    "SAT",
+    "SUN"
+};
+
 struct timer_t {
     uint64_t prev_time_ms;
     uint64_t current_time_ms;
@@ -79,11 +98,13 @@ struct lcd_position {
 };
 
 struct rtc_date_t {
-    char str_date[11]; /* 10 characters for the date + null character */
+    /* 10 characters for the date + space + 3 characters for the day of the week + null character */
+    char str_date[15];
 
     uint8_t date;
     uint8_t month;
     uint8_t year;
+    uint8_t day_week;
 
     uint8_t str_lcd_col;
     uint8_t str_lcd_row;
@@ -91,10 +112,11 @@ struct rtc_date_t {
     lcd_position pos_date_date;
     lcd_position pos_date_month;
     lcd_position pos_date_year;
+    lcd_position pos_day_week;
 }rtc_date;
 
 struct rtc_time_t {
-    char str_time[9]; /* 8 characters for the date + null character */
+    char str_time[9]; /* 8 characters for the time + null character */
 
     uint8_t hour;
     uint8_t min;
@@ -108,10 +130,11 @@ struct rtc_time_t {
     lcd_position pos_time_sec;
 }rtc_time;
 
-struct lcd_position *date_time_positions[6] = {
+struct lcd_position *date_time_positions[7] = {
     &rtc_date.pos_date_date,
     &rtc_date.pos_date_month,
     &rtc_date.pos_date_year,
+    &rtc_date.pos_day_week,
     &rtc_time.pos_time_hr,
     &rtc_time.pos_time_min,
     &rtc_time.pos_time_sec
@@ -133,11 +156,12 @@ void setup(void)
     rtc_date.date = 0;
     rtc_date.month = 0;
     rtc_date.year = 0;
-    rtc_date.str_lcd_col = 3;
+    rtc_date.str_lcd_col = 1;
     rtc_date.str_lcd_row = 0;
-    rtc_date.pos_date_date = {3, 0};
-    rtc_date.pos_date_month = {6, 0};
-    rtc_date.pos_date_year = {11, 0};
+    rtc_date.pos_date_date = {1, 0};
+    rtc_date.pos_date_month = {4, 0};
+    rtc_date.pos_date_year = {9, 0};
+    rtc_date.pos_day_week = {12, 0};
 
     rtc_time.str_time[9] = {0};
     rtc_time.hour = 0;
@@ -156,6 +180,7 @@ void setup(void)
     rtc_date.date = rtc.getDate();
     rtc_date.month = rtc.getMonth(century);
     rtc_date.year = rtc.getYear();
+    rtc_date.day_week = rtc.getDoW();
 
     display_date_time();
 }
@@ -189,7 +214,9 @@ void display_date_time(void)
     }
 
     sprintf(rtc_time.str_time, "%02u:%02u:%02u", rtc_time.hour, rtc_time.min, rtc_time.sec);
-    sprintf(rtc_date.str_date, "%02u/%02u/20%02u", rtc_date.date, rtc_date.month, rtc_date.year);
+    sprintf(rtc_date.str_date, "%02u/%02u/20%02u %s",
+                rtc_date.date, rtc_date.month, rtc_date.year,
+                str_days_week[rtc_date.day_week]);
 
     lcd.setCursor(rtc_date.str_lcd_col,rtc_date.str_lcd_row);
     lcd.print(rtc_date.str_date);
@@ -200,23 +227,23 @@ void display_date_time(void)
 
 void edit_date_time(void)
 {
-    byte rtc_current[6] = {
+    byte rtc_current[7] = {
         rtc_date.date,
         rtc_date.month,
         rtc_date.year,
+        rtc_date.day_week,
         rtc_time.hour,
         rtc_time.min,
         rtc_time.sec
     };
 
-    char str_param[3] = {0};
+    /* 2 chars for date or time, 3 chars for day of the week + null character */
+    char str_param[4] = {0};
 
-    uint8_t param_idx = 0;
+    uint8_t param_idx = DATE_IDX;
     uint8_t tmp_lcd_col = 0;
     uint8_t tmp_lcd_row = 0;
     uint8_t date_time_flags = 0x00;
-
-    sprintf(str_param, "%02u", rtc_current[param_idx]);
 
     reset_timer(&timer, BLINKING_PARAM_MS);
 
@@ -226,7 +253,14 @@ void edit_date_time(void)
 
         tmp_lcd_col = date_time_positions[param_idx]->col;
         tmp_lcd_row = date_time_positions[param_idx]->row;
-        blink_parameter(tmp_lcd_col, tmp_lcd_row, str_param);
+
+        if(param_idx == WEEK_IDX) {
+            sprintf(str_param, "%s", str_days_week[rtc_current[param_idx]]);
+        } else {
+            sprintf(str_param, "%02u", rtc_current[param_idx]);
+        }
+
+        blink_parameter(param_idx, tmp_lcd_col, tmp_lcd_row, str_param);
 
         /* Edit the next parameter on the LCD after the button edit is clicked */
         if(btn_edit.isClicked() == true) {
@@ -235,7 +269,6 @@ void edit_date_time(void)
             lcd.print(str_param);
 
             param_idx += 1;
-            sprintf(str_param, "%02u", rtc_current[param_idx]);
 
             btn_edit.resetClicked();
             continue;
@@ -245,7 +278,6 @@ void edit_date_time(void)
         if (btn_set.isClicked() == true) {
             increment_param(param_idx, &rtc_current[param_idx], &date_time_flags);
             
-            sprintf(str_param, "%02u", rtc_current[param_idx]);
             lcd.setCursor(tmp_lcd_col, tmp_lcd_row);
             lcd.print(str_param);
             
@@ -300,9 +332,9 @@ void update_date_time(uint8_t date_time_flags, uint8_t *current_date_time)
     }
 }
 
-void blink_parameter(uint8_t lcd_col, uint8_t lcd_row, char *str_param)
+void blink_parameter(uint8_t param_idx, uint8_t lcd_col, uint8_t lcd_row, char *str_param)
 {
-    const char *str_blank_space = "  ";
+   char str_blank_space[4] = {0}; 
     static uint8_t param_txt_flag = 0x01;
  
     timer_timeout(&timer);
@@ -312,8 +344,9 @@ void blink_parameter(uint8_t lcd_col, uint8_t lcd_row, char *str_param)
 
     timer.timeout = RESET_TIMER_VALUE;
     param_txt_flag ^= 1;
-    
+
     lcd.setCursor(lcd_col, lcd_row);
+    (param_idx != WEEK_IDX) ? sprintf(str_blank_space, "%s", "  ") : sprintf(str_blank_space, "%s", "   ");
     (param_txt_flag == BLINK_CLEAR_PARAM) ? lcd.print(str_blank_space) : lcd.print(str_param);
 }
 
