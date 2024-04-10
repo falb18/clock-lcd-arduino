c80966a3fe6174671453098946a5462c7cc6a8a9
falb18 <arturo.lopezbo@gmail.com>
2024-04-09 19:06:28 -0600

    Add TODO comment on timer_module

--- timer_module.cpp
+++ timer_module.cpp
@@ -18,4 +18,9 @@ void timer_timeout(struct timer_t *tmr)
         tmr->timeout = true;
         tmr->prev_time_ms = tmr->current_time_ms;
     }
+
+    /* TODO: Cover the case when internal timer has overflowed and hence the prev_time_ms is greater than
+     * current_time_ms. For that particular case, math operation are needed to verify that the corresponding
+     * time has elapsed.
+     */
 }
